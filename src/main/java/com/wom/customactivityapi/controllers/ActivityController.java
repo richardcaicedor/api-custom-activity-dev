18c6b174243af68615a221593cb075f7256a837c
richardcaicedor <31871319+richardcaicedor@users.noreply.github.com>
2024-05-27 22:16:23 -0500

    Update ActivityController.java

--- src/main/java/com/wom/customactivityapi/controllers/ActivityController.java
+++ src/main/java/com/wom/customactivityapi/controllers/ActivityController.java
@@ -43,6 +43,21 @@ public class ActivityController {
         }
     } 
 
+    @PostMapping("/save")
+    public ResponseEntity<String> save (@RequestBody Map<String, Object> payload) {
+        return new ResponseEntity<>("Save", HttpStatus.OK);
+    }
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publish (@RequestBody Map<String, Object> payload) {
+        return new ResponseEntity<>("Publish", HttpStatus.OK);
+    }
+
+    @PostMapping("/stop")
+    public ResponseEntity<String> stop (@RequestBody Map<String, Object> payload) {
+        return new ResponseEntity<>("Stop", HttpStatus.OK);
+    }
+
     @PostMapping("/validate") 
     public ResponseEntity<String> validate (@RequestBody Map<String, Object> payload) {
         return new ResponseEntity<>("Validate", HttpStatus.OK);
